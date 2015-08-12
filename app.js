6a2251b5918c73ed7a18082e8683a9eef4c0fcb5
Paul Showalter <poeslacker@gmail.com>
2015-08-12 16:44:12 -0700

    color picker saves

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var save_show = require('./routes/save_show');
+var save_color = require('./routes/save_color');
 
 var app = express();
 
 var redis = require('redis');
-var client = redis.createClient(); //creates a new client
+var redis_client = redis.createClient(); //creates a new client
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.post('/save_show', save_show);
+app.post('/save_color', save_color);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,3 +68,24 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var OPC = new require('./opc')
+var client = new OPC('localhost', 7890);
+
+function draw() {
+    var millis = new Date().getTime();
+
+    for (var pixel = 0; pixel < 512; pixel++)
+    {
+        var t = pixel * 0.2 + millis * 0.002;
+        var red = 128 + 96 * Math.sin(t);
+        var green = 128 + 96 * Math.sin(t + 0.1);
+        var blue = 128 + 96 * Math.sin(t + 0.3);
+
+        client.setPixel(pixel, red, green, blue);
+    }
+    client.writePixels();
+}
+
+setInterval(draw, 30);
+
