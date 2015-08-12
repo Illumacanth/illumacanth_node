3a51c757385a69542a42cf8075a481bb17f61443
Paul Showalter <poeslacker@gmail.com>
2015-08-12 12:50:07 -0700

    added redis

--- illumacanth.js
+++ illumacanth.js
@@ -1,9 +1,30 @@
+
+
+var redis = require('redis');
+var client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('connected');
+});
+
+
 var express = require('express');
 
 var server = express();
 server.use(express.static(__dirname + '/public'));
 
 var port = 10001;
+
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
+
+app.post('/save', routes.saveTodo);
+
+exports.saveTodo = function(req, res) {
+  var newTodo = {};
+  newTodo.name = req.body['todo-text'];
+  newTodo.id = newTodo.name.replace(" ", "-");
+  client.hset("Todo", newTodo.id, newTodo.name);
+  res.redirect("back");
+};
