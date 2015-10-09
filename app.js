4f15d66f870919ac8826e90547cedfcaa7a6afd4
Paul Showalter <poeslacker@gmail.com>
2015-10-08 22:11:30 -0700

    merge resolution

--- app.js
+++ app.js
@@ -161,11 +161,6 @@ function draw() {
     });
 }
 
-      function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
-      function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
-      function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
-      function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
-
 var time = 0;
 
 function drawnew(default_leds) {
@@ -223,65 +218,4 @@ redis_client.mget(['default_leds'], function(err, reply) {
   setInterval(function(){draw(default_leds)}, 100);
 });
 
-
-/* accepts parameters
- * h  Object = {h:x, s:y, v:z}
- * OR
- * h, s, v
-*/
-      function HSVtoRGB(h, s, v) {
-            var r, g, b, i, f, p, q, t;
-            if (arguments.length === 1) {
-                s = h.s, v = h.v, h = h.h;
-            }
-            i = Math.floor(h * 6);
-            f = h * 6 - i;
-            p = v * (1 - s);
-            q = v * (1 - f * s);
-            t = v * (1 - (1 - f) * s);
-            switch (i % 6) {
-                case 0: r = v, g = t, b = p; break;
-                case 1: r = q, g = v, b = p; break;
-                case 2: r = p, g = v, b = t; break;
-                case 3: r = p, g = q, b = v; break;
-                case 4: r = t, g = p, b = v; break;
-                case 5: r = v, g = p, b = q; break;
-            }
-          return {
-              r: Math.round(r * 255),
-              g: Math.round(g * 255),
-              b: Math.round(b * 255)
-          };
-      };
-
-/* accepts parameters
- * r  Object = {r:x, g:y, b:z}
- * OR
- * r, g, b
-*/
-function RGBtoHSV(r, g, b) {
-    if (arguments.length === 1) {
-        g = r.g, b = r.b, r = r.r;
-    }
-    var max = Math.max(r, g, b), min = Math.min(r, g, b),
-        d = max - min,
-        h,
-        s = (max === 0 ? 0 : d / max),
-        v = max / 255;
-
-    switch (max) {
-        case min: h = 0; break;
-        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
-        case g: h = (b - r) + d * 2; h /= 6 * d; break;
-        case b: h = (r - g) + d * 4; h /= 6 * d; break;
-    }
-
-    return {
-        h: h,
-        s: s,
-        v: v
-    };
-};
-
-
-
+var color_math = require('./color_math.js');
